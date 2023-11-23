a17701183ba986ee5ce6474caa191425726a4b4e
Alejandra Evander <alejandra.evander92@gmail.com>
2023-11-23 15:52:22 +0800

    Second commit

--- .selenium_training/Scripts/selenium_1.py
+++ .selenium_training/Scripts/selenium_1.py
@@ -1,1 +1,17 @@
-# //tagName
+# //tagName[@AttributName="Value"]
+# contains() / starts-with()
+# //tagName[@contains(@AttributeName, "Value")]
+# //tagName[(expression 1) and (expression 2)]\
+# // parent / single, . present, .. parent, //x/" all details in x
+# Selenium can scrap website with dynamic content
+
+from selenium import webdriver
+
+website = "https://www.adamchoi.co.uk/overs/detailed"
+path = r"C:\Users\r14ale\Desktop\Selenium\.selenium_training\msedgedriver.exe"
+
+driver = webdriver.Edge(path)
+driver.get(website)
+
+driver.quit()
+
