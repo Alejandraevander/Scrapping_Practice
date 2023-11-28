9316ae2bf1a46c5acac78abccdf72c3bc3241862
Alejandra Evander <alejandra.evander92@gmail.com>
2023-11-28 14:25:02 +0800

    Headless & Scroll

--- .selenium_training/Scripts/selenium_3_Section_7.py
+++ .selenium_training/Scripts/selenium_3_Section_7.py
@@ -4,7 +4,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
-web = "https://www.audible.com/search"
+
+#Website to Scrap and driver location
+web = "https://twitter.com/search?q=python&src=typed_query"
 path = r"C:\\Users\\r14ale\\Desktop\\Scrapping_Practice\\.selenium_training\\msedgedriver.exe"
 
 #Compulsory Code in Selenium 4.0
@@ -13,25 +15,23 @@ service = Service(path)
 
 #Code to Disable the Devtools listening on ....
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-options.add_argument("--headless=new")
+#options.add_argument("--headless=new")
 options.add_argument("windows-size=1920x1080")
 #Open MS Edge webdriver and opening the website
 driver = webdriver.Edge(service=service, options=options)
 driver.get(web)
-#driver.maximize_window()
 
-container = driver.find_element(By.CLASS_NAME, 'adbl-impression-container')
-products = container.find_element(By.XPATH, '*/li[contains(@class, "productListItem")]')
+#To Scroll Infinite Scrolling
+last_height = driver.execute_script("return document.body.scrollHeight")
 
-title=[]
-author=[]
-length=[]
+while True:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(5)
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    else:
+        last_height = new_height
+login = driver.find_element(By.XPATH, "//a[@href='/login']")
 
-for product in products:
-    title.append(product.find_element(By.XPATH, "//h3[contains(@class, 'bc-heading')]").text)
-    author.append(product.find_element(By.XPATH, "//LI[contains(@class, 'authorLabel')]").text)
-    length.append(product.find_element(By.XPATH, "//LI[contains(@class, 'RUNTIMELabel')]").text)
-    
-driver.quit()
-df = pd.DataFrame({'title':title,'author':author,'length':length})
-df.to_csv('Selenium_2.csv', index=False)
+//input[@name='text']
